f6f3148fe232710ab586bd926c125978c03e9540
Goa Pride <admin@goapride.com>
2023-07-14 19:52:02 +0530

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <title>Goa Pride - LGBTQ+ friendly travel information and tours</title>
-</head>
-<body style="text-align:center; padding:50px">
-    <h1>Goa Pride - LGBTQ+ travel information</h1>
-    <p>This Website is under construction</p>
-</body>
-</html>
